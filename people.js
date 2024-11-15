861207b5280b1e6c0c5905b8c2c373bdf500240e
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-15 17:13:04 +0100

    add: import functions in people.js

---
+++ people.js
@@ -0,0 +1,7 @@
+/* 3 - people.js
+Importa la tua funzione da names.js
+Importa la tua funzione da hobbies.js
+Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies. All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto. */
+
+const signature = require("./names.js"); // importo la funzione nameandsurname da names.js
+const hobby = require("./hobbies.js"); // importo la funzione choiceHobbies da hobbies.js
